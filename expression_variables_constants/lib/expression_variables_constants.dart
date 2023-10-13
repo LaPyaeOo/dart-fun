2961ea081d57293b1daf9f1c485e4f3adb24e9fb
LaPyae <lapyaeoo@ayainnovation.com>
2023-10-13 23:44:15 +0630

    add: add comppletely answer for quadraticEquation challenge

--- expression_variables_constants/lib/expression_variables_constants.dart
+++ expression_variables_constants/lib/expression_variables_constants.dart
@@ -9,15 +9,19 @@ int computeTheAnswer() {
 
 /// if squart() is negative, then will return NAN. So check whether (-) or not
 double quadraticEquation() {
-  const double a = 20;
-  const double b = 200;
+  const double a = 1;
+  const double b = 10;
   const double c = 3;
-  double x;
+  double xWithPlus;
+  double xWithMinus;
   double powerTemp = pow(b, 2) - (4 * a * c);
   if (powerTemp < 0) {
     return 0;
   } else {
-    x = (-b - sqrt(powerTemp) / (2 * a * c));
+    xWithPlus = (-b + sqrt(powerTemp) / (2 * a * c));
+    xWithMinus = (-b - sqrt(powerTemp) / (2 * a * c));
   }
-  return x;
+  double root1 = a * pow(xWithPlus, 2) + b * xWithPlus + c;
+  double root2 = a * pow(xWithMinus, 2) + b * xWithPlus + c;
+  return root2;
 }
