6bcc85164720ec0ecc7ae566671c8b4f1e7d7751
LaPyae <lapyaeoo@ayainnovation.com>
2023-11-12 00:44:01 +0630

    fix: remove deleted files from git log

--- getting_character/bin/getting_character.dart
+++
@@ -1,5 +0,0 @@
-import 'package:getting_character/getting_character.dart' as getting_character;
-
-void main(List<String> arguments) {
-  print('Hello world: ${getting_character.calculate()}!');
-}
--- getting_character/test/getting_character_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:getting_character/getting_character.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
